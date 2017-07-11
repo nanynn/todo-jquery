8d57dbd97ea0eebd220843e8ec9b12fe4c099228
Anayn Pavez <nanyn.pavez@gmail.com>
2017-07-10 23:00:52 -0300

    funcionalidad con jquery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,29 @@
 $(document).ready(function(){
 	$('#btn-agregar').click(function(){
 		var tareas = $('input').val();
-		$('.tareas-nuevas').append('<div class=".tareas-nuevas">' + tareas + '</div>'); 
+		if (tareas =="") {
+			alert('Ingrese una tarea');
+		} else{
+			$('#misTareas').append('<li><input type="checkbox" id="test6" checked="checked"/><label for="test6">' + tareas +
+      					'</label><button class="rmvBtn btn-floating">x</button></li>');
+	    	tareas;
+		}
+		//$('.tareas-nuevas').append('<div class=".tareas-nuevas">' + tareas + '</div>'); 
+		$('.check').on('click', marcar);
+		$('.rmvBtn').on('click', borrar);
 	});
 });
 
-$(document).on('click', '.tareas-nuevas', function() {
-    $(this).remove()
-});
+function borrar(){
+    $(this).closest('li').remove();
+}
+
+function marcar(){
+	if(this.check){
+		$(this).closest('li').addClass('done');
+	} else{
+		$(this).closest('li').removeClass('done');
+	}
+}
+
+
